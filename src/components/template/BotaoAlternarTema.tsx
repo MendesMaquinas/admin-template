4300bceae065f56920ebf7177885e7b89603362a
MendesMaquinas <api.mendes@mendesmaquinas.com.br>
2023-08-23 17:44:36 -0300

    up funcionamento de botao

--- src/components/template/BotaoAlternarTema.tsx
+++ src/components/template/BotaoAlternarTema.tsx
@@ -1,14 +1,14 @@
 import { IconeMoon, IconeSun } from '../icons/Index';
 
 interface BotaoAlternarTemaProps {
-  tema: string;
-  alternarTema: () => void;
+  tema: string,
+  alternarTema: () => void,
 }
 
 export default function BotaoAlternarTema(props: BotaoAlternarTemaProps) {
   return props.tema === 'dark' ? (
     <div
-      onClick={() => props.alternarTema}
+      onClick={props.alternarTema}
       className="hidden sm:flex items-center cursor-pointer bg-gradient-to-r from-yellow-300 to-yellow-600 w-14 lg:w-24 h-8 p-1 rounded-full"
     >
       <div className="flex items-center justify-center bg-white text-yellow-600 w-6 h-6 rounded-full">{IconeSun(5)}</div>
@@ -18,7 +18,7 @@ export default function BotaoAlternarTema(props: BotaoAlternarTemaProps) {
     </div>
   ) : (
     <div
-      onClick={() => props.alternarTema}
+      onClick={props.alternarTema}
       className="hidden sm:flex items-center justify-end cursor-pointer bg-gradient-to-r from-indigo-500 to-gray-400 w-14 lg:w-24 h-8 p-1 rounded-full"
     >
       <div className="hidden lg:flex items-center mr-2 text-white">
