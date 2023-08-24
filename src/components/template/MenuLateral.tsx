e6d82598a1cdb6a19ce30441d87442a755e2e38d
MendesMaquinas <api.mendes@mendesmaquinas.com.br>
2023-08-24 17:53:37 -0300

    up

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -2,8 +2,11 @@ import MenuItem from './MenuItem';
 
 import { IconeHome, IconeAjustes, IconeNotificacao, IconeSair } from '../icons/Index';
 import Logo from './Logo';
+import useAuth from '../../data/hook/useAuth';
 
 export default function MenuLateral() {
+
+  const { logout } = useAuth()
   return (
     <aside className='flex flex-col bg-gray-200 text-gray-700 dark:bg-gray-900'>
       <div className="h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800 flex flex-col items-center justify-center">
@@ -15,7 +18,7 @@ export default function MenuLateral() {
         <MenuItem url="/Notificacoes" texto="Notificações" icone={IconeNotificacao} />
       </ul>
       <ul >
-        <MenuItem url="/Autenticacao" texto="Sair" icone={IconeSair} className='text-red-600 dark:text-red-400 hover:bg-red-400 hover:text-white dark:hover:text-white'/>
+        <MenuItem  texto="Sair" icone={IconeSair} onClick={() => logout} className='text-red-600 dark:text-red-400 hover:bg-red-400 hover:text-white dark:hover:text-white'/>
       </ul> 
     </aside>
   );
--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,12 +1,13 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import firebase from '../../firebase/config';
 import Usuario from '../../model/Usuario';
 import route from 'next/router';
-import Cookies from "js-cookie";
+import Cookies from 'js-cookie';
 
 interface AuthContextProps {
   usuario?: Usuario;
   loginGoogle?: () => Promise<void>;
+  logout?: () => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextProps>({});
@@ -24,13 +25,13 @@ async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuar
 }
 
 function gerenciarCookie(logado: boolean) {
-    if (logado) {
-        Cookies.set("admin-template-cod3r-auth", logado, {
-            expires: 1,
-        })
-    } else {
-        Cookies.remove("admin-template-cod3r-auth");
-    }
+  if (logado) {
+    Cookies.set('admin-template-cod3r-auth', logado + '', {
+      expires: 1,
+    });
+  } else {
+    Cookies.remove('admin-template-cod3r-auth');
+  }
 }
 
 export function AuthProvider(props) {
@@ -38,32 +39,54 @@ export function AuthProvider(props) {
   const [usuario, setUsuario] = useState<Usuario>(null);
 
   async function configurarSessao(usuarioFirebase: firebase.User) {
-    if(usuarioFirebase?.email) {
-        const usuario = await usuarioNormalizado(usuarioFirebase);
-        setUsuario(usuario);
-        setCarregando(false);
-        gerenciarCookie(true);
+    if (usuarioFirebase?.email) {
+      const usuario = await usuarioNormalizado(usuarioFirebase);
+      setUsuario(usuario);
+      gerenciarCookie(true);
+      setCarregando(false);
+      return usuario.email;
     } else {
-        setUsuario(null);
-        gerenciarCookie(false);
+      setUsuario(null);
+      gerenciarCookie(false);
+      setCarregando(false);
+      return false;
     }
   }
 
   async function loginGoogle() {
-    const resp = await firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
-    if (resp.user?.email) {
-      const usuario = await usuarioNormalizado(resp.user);
-      setUsuario(usuario);
-
+    try {
+        setCarregando(true);
+      const resp = await firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
+      configurarSessao(resp.user);
       route.push('/');
+    } finally {
+      setCarregando(false);
     }
   }
+
+  async function logout() {
+    try {
+      setCarregando(true);
+      await firebase.auth().signOut();
+      await configurarSessao(null);
+    } finally {
+      setCarregando(false);
+    }
+  }
+
+  useEffect(() => {
+    if (Cookies.get('admin-template-cod3r-auth')) {
+        const cancelar = firebase.auth().onIdTokenChanged(configurarSessao);
+        return () => cancelar();
+    }
+  }, []);
 
   return (
     <AuthContext.Provider
       value={{
         usuario,
         loginGoogle,
+        logout,
       }}
     >
       {props.children}
